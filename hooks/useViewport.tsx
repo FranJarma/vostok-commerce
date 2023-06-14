99681de257fb549c9d84b65aee031a3b2f3296c7
Francisco Jarma <francisco.jarma@onetree.com>
2023-06-14 16:20:04 -0300

    Added validation to check if we are in SSR

--- hooks/useViewport.tsx
+++ hooks/useViewport.tsx
@@ -1,13 +1,22 @@
 "use client"
 import { useState, useEffect } from 'react'
 export const useViewport = () => {
-    const [width, setWidth] = useState<number>(window.innerWidth);
-  
-    useEffect(() => {
-      const handleWindowResize = () => setWidth(window.innerWidth);
+  const [width, setWidth] = useState<number>(0);
+
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+
+    if (typeof window !== 'undefined') {
+      setWidth(window.innerWidth);
       window.addEventListener("resize", handleWindowResize);
-      return () => window.removeEventListener("resize", handleWindowResize);
-    }, []);
-  
-    return { width };
+    }
+
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener("resize", handleWindowResize);
+      }
+    };
+  }, []);
+
+  return { width };
 }
